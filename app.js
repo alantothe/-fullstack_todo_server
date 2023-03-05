f03b1eb3e3453666c07090f0313eeae73593f75f
alantothe <rubenamalpartida@gmail.com>
2023-03-04 23:06:37 -0500

    Basic Scaffolding

--- app.js
+++ app.js
@@ -6,19 +6,20 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const todosRouter = require('./routes/todosRouter'); // import todosRouter
 
 var app = express();
 
 const cors = require("cors");
 require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 var { mongoConnect } = require('./mongo.js');
 mongoConnect();
-
-var app = express();
 
 app.use(cors());
 app.options("*", cors());
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', todosRouter); // add todosRouter at "/api" endpoint
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
